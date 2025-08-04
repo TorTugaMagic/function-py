5f7ed5e5c94680457cb8d6bf071dbf143ddafdc7
TorTugaMagic <205472562+TorTugaMagic@users.noreply.github.com>
2025-08-04 01:51:31 +0000

    Give up a container in the cloud or in your computer, with microservices

--- save.sh
+++ save.sh
@@ -4,7 +4,7 @@
 
 git add mylib git keyTerm M2
 git add .
-git commit -m "Corriengo error de lint test reaady, formato"
+git commit -m "Give up a container in the cloud or in your computer, with microservices"
 git push origin main
 echo "Archivo git/guardar.sh agregado y enviado al repositorio."
 echo "para ejecutar el script, usa el comando:bash git/guardar.sh"
